542282b1e4b8165862fe4d5e476aeb4a5bd51dc9
Gokul Raj <vgokulraj@live.com>
2019-05-07 16:19:45 +0530

    changes

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -8,7 +8,8 @@ class Main extends Component{
     render(){
         return(
             <div className="middleContainer" >
-                <TopMenu /> 
+{/* <iframe frameborder='0'  width='500' height='500'  title='Tamil Medium 1st Standard Environmental studies' src='http://online.pubhtml5.com/fjxl/wxvq/' type='text/html' allowfullscreen='true' scrolling='no' marginwidth='0' marginheight='0'></iframe>               */}
+                  <TopMenu /> 
                     <LeftMenu />
                     <Content/>
                     <RightMenu/>
